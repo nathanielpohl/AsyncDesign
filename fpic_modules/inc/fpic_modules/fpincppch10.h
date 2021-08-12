eabdab72a64ddc2aede16fa0bff2a5f8b7bd5d10
Nathaniel Pohl <nathaniel.pohl@gmail.com>
2021-08-12 11:59:22 -0600

    forgot a file

---
+++ fpic_modules/inc/fpic_modules/fpincppch10.h
@@ -0,0 +1,21 @@
+// This file contains the class derivative for a playing around with functional
+// programming.
+#pragma once
+
+#include <string>
+
+#include "tools/command.h"
+#include "tools/csv_parser.h"
+
+namespace fpic_modules {
+class FPInCppCh10 : public tools::Command {
+ public:
+  FPInCppCh10(std::string command_id);
+  FPInCppCh10* Clone() { return new FPInCppCh10(command_id_); }
+  void Deserialize(tools::CSVParser& params);
+  int Execute();
+
+ private:
+};
+
+}  // namespace fpic_modules
